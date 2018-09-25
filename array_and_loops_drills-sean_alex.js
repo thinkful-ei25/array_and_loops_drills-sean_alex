1a49a22b59d57c5b738f3df1380b7789cd5a7d32
talexcrowell <talexcrowell@gmail.com>
2018-09-25 09:39:51 -0700

    added function fizzbuzz

--- array_and_loops_drills-sean_alex.js
+++ array_and_loops_drills-sean_alex.js
@@ -42,3 +42,20 @@ function average(arr) {
 const arr = [3, 4, 5, 6]; 
 const testAvg = average(arr); 
 console.log(testAvg); 
+
+function fizzBuzz(countTo) {
+ return new Array(countTo).fill(1).map((value, index) => index + 1).map(value => { 
+  if (value % 3 === 0 && value % 5 ===0) {
+    return 'fizzbuzz';
+  } else if (value % 5 === 0) {
+    return 'buzz';
+  } else if (value % 3 === 0) {
+    return 'fizz';
+  } else {
+    return value;
+  }
+  });
+}
+
+const fizzTest = fizzBuzz(15);
+console.log(fizzTest);
